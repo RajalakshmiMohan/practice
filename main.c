c0b9771b32e8f2c0a487ca4ad81baff5ec23ff3d
RajalakshmiMohan <rajalakshmi15mohan@gmail.com>
2016-06-30 12:55:52 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -2,19 +2,29 @@
 
 int main()
 {
+	int pal,num;
 	
-	int i,num;
-	unsigned long int fact=1;
-	i=1;
-	printf("Enter any number but not beyond 34 \n");
+	int rev=0;
+	printf("Enter the no to be checked for palindrome pattern \n");
 	scanf("%d",&num);
+	pal=num;
+	//num=pal;
+	while(num!=0){
+		
 	
-	while(i<=num){
-		fact=fact*i;
-			i++;
-	
-	
+	rev=rev*10;
+	rev=rev+num%10;
+	num=num/10;
+
 	}
-	printf("The factorial of the given number is %d \n",fact);
+	printf("The number is reversed and is %d and check the result for palindrome too\n",rev);
+	//scanf("%d",&rev);
+	if(pal==rev){
+		printf("The number is a palindrome patterned \n");
+		
+	}
+	else{
+		printf("Sorry,a very bad number :-( \n");
+	}
 	return 0;
 }
